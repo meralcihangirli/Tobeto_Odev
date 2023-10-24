8ec6b1405d8ac303cf41401295cd354320c195d3
meralcihangirli <mcihangirli@gmail.com>
2023-10-25 01:08:30 +0300

    Bool Ver, Tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -12,6 +12,7 @@ namespace TypesAndVariables
         {
             //Value Types
             //Console.WriteLine("Hello World!");
+            bool condition = false;
             byte number4 = 255;
             short number3 = 32767;
             int number1 = 2147483647;

f00c221ad7cdf451b84ca98d3b2f956b0a028754
meralcihangirli <mcihangirli@gmail.com>
2023-10-25 01:04:36 +0300

    Long Veri Tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -13,7 +13,10 @@ namespace TypesAndVariables
             //Value Types
             //Console.WriteLine("Hello World!");
             int number1 = 2147483647;
+            long number2 = 9223372036854775807;
+
             Console.WriteLine("Number1 is {0}", number1);
+            Console.WriteLine("Number2 is {0}", number2);
 
 
             Console.ReadLine();

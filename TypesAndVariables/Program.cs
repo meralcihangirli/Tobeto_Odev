5d6c0fb5ef8139cb2d193437874d735cc5763c1c
meralcihangirli <mcihangirli@gmail.com>
2023-10-25 01:06:11 +0300

    Short Veri Tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -12,18 +12,19 @@ namespace TypesAndVariables
         {
             //Value Types
             //Console.WriteLine("Hello World!");
+            short number3 = 32767;
             int number1 = 2147483647;
             long number2 = 9223372036854775807;
 
             Console.WriteLine("Number1 is {0}", number1);
             Console.WriteLine("Number2 is {0}", number2);
-
+            Console.WriteLine("Number3 is {0}", number3);
 
             Console.ReadLine();
-            
+
         }
     }
 
-    
+
 }
 

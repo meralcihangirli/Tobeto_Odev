816c0833222e25e51e3299e5f5fe2aaeca92d02a
meralcihangirli <mcihangirli@gmail.com>
2023-10-25 01:07:08 +0300

    Byte Veri Tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -12,6 +12,7 @@ namespace TypesAndVariables
         {
             //Value Types
             //Console.WriteLine("Hello World!");
+            byte number4 = 255;
             short number3 = 32767;
             int number1 = 2147483647;
             long number2 = 9223372036854775807;
@@ -19,6 +20,8 @@ namespace TypesAndVariables
             Console.WriteLine("Number1 is {0}", number1);
             Console.WriteLine("Number2 is {0}", number2);
             Console.WriteLine("Number3 is {0}", number3);
+            Console.WriteLine("Number4 is {0}", number4);
+
 
             Console.ReadLine();
 

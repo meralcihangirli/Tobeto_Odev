7732847f40f31401e4e66bec392a1a1ff1ab5661
meral cihangirli <blacksoul1907@hotmail.com>
2023-10-24 20:18:40 +0300

    While Döngüsü

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -10,19 +10,35 @@ namespace Loops
     internal class Program
     {
         static void Main(string[] args)
-        { 
+        {
             //for döngüsü  
-            for (int i = 1; i <=100; i++)
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine("Finished!");
+            //Console.ReadLine();
+
+
+            //ForLoop();
+            int number = 100;
+            while (number >= 0)
+            {
+                Console.WriteLine(number);
+                number--;
+            }
+            Console.WriteLine("Now number is {0}", number);
+            Console.ReadLine();
+        }
+
+        private static void ForLoop()
+        {
+            for (int i = 100; i >= 0; i = i - 2)
             {
                 Console.WriteLine(i);
             }
             Console.WriteLine("Finished!");
-            Console.ReadLine();
-
-            
         }
-        
-
 
     }
 }

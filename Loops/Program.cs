b8b244c71bcd4291e7ff406c9c45d29f82f13801
meral cihangirli <blacksoul1907@hotmail.com>
2023-10-24 19:29:43 +0300

    For Döngüsü

---
+++ Loops/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            //for döngüsü  
+            for (int i = 1; i <=100; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Finished!");
+            Console.ReadLine();
+
+            
+        }
+        
+
+
+    }
+}

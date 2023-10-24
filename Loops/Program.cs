dbde5084f510d5e03681d3003f8e7b106200ae44
meral cihangirli <blacksoul1907@hotmail.com>
2023-10-24 20:35:11 +0300

    Do While Döngüsü

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -21,24 +21,48 @@ namespace Loops
 
 
             //ForLoop();
+            //int number = 100;
+            //while (number >= 0)
+            //{
+            //    Console.WriteLine(number);
+            //    number--;
+            //}
+            //Console.WriteLine("Now number is {0}", number);
+            //Console.ReadLine();
+
+
+            //WhileLoop();
+            int number = 10;
+            do
+            {
+                Console.WriteLine(number);
+                number--;
+            } while (number >= 11);
+            Console.ReadLine();
+
+
+        }
+
+        //private static void ForLoop()
+        //{
+        //    for (int i = 100; i >= 0; i = i - 2)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+        //    Console.WriteLine("Finished!");
+        //}
+
+        private static void WhileLoop()
+        {
             int number = 100;
             while (number >= 0)
             {
                 Console.WriteLine(number);
                 number--;
             }
-            Console.WriteLine("Now number is {0}", number);
-            Console.ReadLine();
         }
 
-        private static void ForLoop()
-        {
-            for (int i = 100; i >= 0; i = i - 2)
-            {
-                Console.WriteLine(i);
-            }
-            Console.WriteLine("Finished!");
-        }
+
 
     }
 }

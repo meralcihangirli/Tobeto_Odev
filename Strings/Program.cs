3cdd95db4d2d3e299e0f9203d1d543407d3208f8
meral cihangirli <blacksoul1907@hotmail.com>
2023-10-24 21:46:52 +0300

    Revert "Revert "String ile Çalışmaya Başlamak""
    This reverts commit 6e80b6e17f3300bc21514e171046e1be77dea5be.

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -10,6 +10,50 @@ namespace Strings
     {
         static void Main(string[] args)
         {
+            //string city = "Ankara";
+            ////Console.WriteLine(city[0]);
+
+            //foreach (var item in city)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //string city2 = "İstanbul";
+            ////  string result = city + city2;
+            //// Console.WriteLine(result);
+
+            //Console.WriteLine(string.Format("{0} {1}", city, city2));
+
+            //Intro();
+            string sentence = "My name is Engin Demirog";
+            var result = sentence.Length;
+            var result2 = sentence.Clone();
+            sentence = "My name is Derin Demirog";
+            bool resul3 = sentence.EndsWith("g");
+            bool result4= sentence.StartsWith ("My name");
+
+            var result5 = sentence.IndexOf("name");
+            var result6=sentence.IndexOf(" ");
+            var result7=sentence.LastIndexOf(" ");
+            var result8=sentence.Insert(0,"Hello, ");
+            //var result9 = sentence.Substring(3);
+            var result9 = sentence.Substring(3,4);
+            var result10=sentence.ToLower();
+            var result11=sentence.ToUpper();
+            var result12=sentence.Replace("","-");
+            //var result13 = sentence.Remove(2);
+            var result13 = sentence.Remove(2,5);
+
+            //Console.WriteLine(result);
+            Console.WriteLine(result13);
+
+
+            Console.ReadLine();
+
+        }
+
+        private static void Intro()
+        {
             string city = "Ankara";
             //Console.WriteLine(city[0]);
 
@@ -22,9 +66,7 @@ namespace Strings
             //  string result = city + city2;
             // Console.WriteLine(result);
 
-            Console.WriteLine(string.Format("{0} {1}",city,city2));
-            Console.ReadLine();
-
+            Console.WriteLine(string.Format("{0} {1}", city, city2));
         }
     }
 }

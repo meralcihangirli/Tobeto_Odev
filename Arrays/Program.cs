bf4daf3a0be89b873c76b3f4ee15ce7ee5f6612c
meralcihangirli <mcihangirli@gmail.com>
2023-10-25 12:56:30 +0300

    Diziler

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -15,11 +15,9 @@ namespace Arrays
             //string student2 = "Derin";
             //string student3 = "Salih";
 
+
             //arraylar; tek bir noktadan aynı tip değişkenleri yönetebiliriz.Array kullanılmadıgında üstteki string ifadeler gibi tek tek tanımlama ile uğrasılacaktır.
             // [] --> tek boyutlu array yazımı
-
-            //array kullanımı 4
-            string[] students = new string[3] { "Engin", "Derin", "Salih" };
 
             //array kullanımı 1
             // string[] students = new string[3];
@@ -27,42 +25,24 @@ namespace Arrays
             //students[1] = "Derin";
             //students[2] = "Salih";
 
-            //array kullanımı 2
-            //string[] students2 = new[] {"Engin","Derin","Salih"};
+            // array kullanımı 2
+            //string[] students2 = new[] { "Engin", "Derin", "Salih" };
 
             //array kullanımı 3
             string[] students2 = { "Engin", "Derin", "Salih" };
             //students2[3] = "Ahmet"; -> students2'ye tanımlanan eleman sayısı 3tür, bu satırdaki de eklenince array sınır sayısını aşacağından program hata verecektir.
 
-
-            //foreach (var student in students2)
-            //{
-            //    Console.WriteLine(student);
-            //}
+            //array kullanımı 4
+            string[] students = new string[3] { "Engin", "Derin", "Salih" };
 
 
-            // [,] --> çok boyutlu array yazımı.Çok boyutlar arraylar 0'dan başlamaz. [5,3] 5 satır 3 sütun anlamına gelmektedir. 
-            string[,] regions = new string[5, 3]
+
+            foreach (var student in students2)
             {
-                { "İstanbul", "İzmit", "Balıkesir" },
-                { "Ankara", "Konya", "Kırıkkale" },
-                { "Antalya", "Adana", "Mersin" },
-                { "Rize", "Trabzon", "Samsun" },
-                { "İzmir", "Muğla", "Manisa" },
-
-            };
-
-            for (int i = 0; i <= regions.GetUpperBound(0); i++)
-            {
-                for (int j = 0; j <= regions.GetUpperBound(1); j++)
-                {
-                    Console.WriteLine(regions[i,j]);
-                }
-                Console.WriteLine("***********");
+                Console.WriteLine(student);
             }
 
-
-            
+            Console.WriteLine();
             Console.ReadLine();
 
         }

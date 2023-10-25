fe5931ff6ef78e02adb384e24bc59510bc3bccc7
meralcihangirli <mcihangirli@gmail.com>
2023-10-25 12:30:25 +0300

    Ref Keyword ile Çalışmak

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -10,12 +10,19 @@ namespace Methods
     {
         static void Main(string[] args)
         {
-            Add();
-            Add();
-            Add();
-            Add();
-            var result=Add2();    
-            Console.WriteLine(result);
+            //Add();
+            //Add();
+            //Add();
+            //Add();
+            //var result = Add2();
+
+            int number1 = 20;
+            int number2 = 100;
+            var result2 = Add3(ref number1, number2);
+
+            //Console.WriteLine(result);
+            Console.WriteLine(result2);
+            Console.WriteLine(number1);
             Console.ReadLine();
         }
         //don't repeat yourself!--> metotların kullanım amaclarından biri kod tekrarına düşmemektir
@@ -25,10 +32,16 @@ namespace Methods
         }
 
         //default deger ataması en sonda olur ama 2den fazlaysa en sagda olandan itibaren sola dogru degerleri atayabilirsin 
-        static int Add2(int number1=20, int number2=30)
+        static int Add2(int number1 = 20, int number2 = 30)
         {
             var result = number1 + number2;
             return result;
+        }
+
+        static int Add3(ref int number1, int number2)
+        {
+            number1 = 30;
+            return number1 + number2;
         }
     }
 

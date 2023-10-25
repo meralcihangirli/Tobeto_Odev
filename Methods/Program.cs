beb7cc1ff383a6d89a4ad2118179e4f72188e8ca
meralcihangirli <mcihangirli@gmail.com>
2023-10-25 12:34:10 +0300

    Out Keyword ile Çalışmak

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -17,8 +17,9 @@ namespace Methods
             //var result = Add2();
 
             int number1 = 20;
+            //int number1; -- out keyword için number1 degerinin metot dısında tanımlı olmaması gerekiyor
             int number2 = 100;
-            var result2 = Add3(ref number1, number2);
+            var result2 = Add3(out number1, number2);
 
             //Console.WriteLine(result);
             Console.WriteLine(result2);
@@ -38,7 +39,11 @@ namespace Methods
             return result;
         }
 
-        static int Add3(ref int number1, int number2)
+        /*ref keyword ; metod ve dışarıdaki degerin basına ref yazılmalı,örn metod içindeki tanımlanan number1 degerinin diger number1 degerine baskın gelmesini istersek geçerli olması ref keywordu kullanarak referans gostermeliyiz*/
+
+        // out keyword ref keyword ile aynı mantıktadır ancak farkı; ref'de metod dısında bulunan number1'in mutlaka bir değeri olmalı ama out için böyle bir zorunluluk bulunmamakta.Out için gönderilecek değerin mutlaka metod içinde set edilmesi gerekir 
+
+        static int Add3(out int number1, int number2)
         {
             number1 = 30;
             return number1 + number2;

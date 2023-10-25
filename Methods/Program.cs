e7109ead4f1eb92677ab64f27f8a96841d64c75e
meralcihangirli <mcihangirli@gmail.com>
2023-10-25 12:18:42 +0300

    Parametreli Metotlarla Çalışmak

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -14,12 +14,20 @@ namespace Methods
             Add();
             Add();
             Add();
+            var result=Add2(20,30);    
+            Console.WriteLine(result);
             Console.ReadLine();
         }
         //don't repeat yourself!--> metotların kullanım amaclarından biri kod tekrarına düşmemektir
         static void Add()
         {
             Console.WriteLine("Added!!!");
+        }
+
+        static int Add2(int number1, int number2)
+        {
+            var result = number1 + number2;
+            return result;
         }
     }
 

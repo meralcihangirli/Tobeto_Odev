6ca8140d455b1d5c5bddaec8c73a84efd1bfb779
meralcihangirli <mcihangirli@gmail.com>
2023-10-26 17:49:06 +0300

    İnterfacelere Temel Giriş

---
+++ Interfaces/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    interface IPerson
+    {
+        int Id { get; set; }
+        int FirstName { get; set; } 
+        int LastName { get; set; }      
+
+    }
+    class Customer : IPerson
+    {
+        public int Id { get; set; }
+        public int FirstName { get; set; }
+        public int LastName { get; set;}
+        public string Adress {  get; set; } 
+    }
+    class Student : IPerson
+    {
+        public int Id { get; set; }
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
+        public string Departmant { get; set; }
+    }
+}

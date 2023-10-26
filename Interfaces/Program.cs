be231b742265f39a751dc12fdefa472f3fd29186
meralcihangirli <mcihangirli@gmail.com>
2023-10-26 19:58:28 +0300

    İnterfaceleri Neden Kullanıyoruz?

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -10,28 +10,83 @@ namespace Interfaces
     {
         static void Main(string[] args)
         {
+            PersonManager manager = new PersonManager();
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Engin",
+                LastName = "Demiroğ",
+                Adress = "Ankara",
+            };
+
+            Student student = new Student
+            {
+                Id = 1,
+                FirstName = "Derin",
+                LastName = "Demiroğ",
+                Departmant = "Computer Sciences",
+            };
+
+
+            manager.Add(customer);
+            manager.Add(student);
+         
+
+            //manager.Add(new Customer
+            //{
+            //    Id = 1,
+            //    FirstName = "Engin",
+            //    LastName = "Demiroğ",
+            //    Adress = "Ankara",
+            //});
+
+            Console.ReadLine();
         }
     }
 
     interface IPerson
     {
         int Id { get; set; }
-        int FirstName { get; set; } 
-        int LastName { get; set; }      
+        string FirstName { get; set; }
+        string LastName { get; set; }
 
     }
     class Customer : IPerson
     {
         public int Id { get; set; }
-        public int FirstName { get; set; }
-        public int LastName { get; set;}
-        public string Adress {  get; set; } 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Adress { get; set; }
     }
     class Student : IPerson
     {
         public int Id { get; set; }
-        public int FirstName { get; set; }
-        public int LastName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Departmant { get; set; }
+    }
+
+    class Worker : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Departmant { get; set; }
+    }
+
+    //class PersonManager
+    //{
+    //    public void Add(Customer customer)
+    //    {
+    //        Console.WriteLine(customer.FirstName);
+    //    }
+    //}
+
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
     }
 }

9587c49d48269fcc574b0b7887aabee92c695893
meralcihangirli <mcihangirli@gmail.com>
2023-10-26 16:33:17 +0300

    Encapsulation

--- Classes/Customer.cs
+++ Classes/Customer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,17 @@ namespace Classes
 {
     internal class Customer
     {
-        public string FirstName;
+        
         public int Id { get; set; }
-        //public string FirstName { get; set; }   
+
+        public string FirstName { get; set; }
+
+        //private string _firstName;
+        //public string FirstName
+        //{
+        //    get { return "Mrs." + _firstName; }
+        //    set { _firstName = value;} 
+        //}   
         public string LastName { get; set; } 
         public string City  { get; set; }
     }
